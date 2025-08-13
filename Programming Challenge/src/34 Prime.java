c763d975fd3c93e96ef3833c7006e20b05c5182b
Ayush <ayusharya.25a@gmail.com>
2025-08-13 14:37:26 +0530

    Update 34 Prime.java

--- Programming Challenge/src/34 Prime.java
+++ Programming Challenge/src/34 Prime.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class Prime {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Welcome to Prime Number checker");
+        System.out.println("Welcome to Prime Number checking system");
         System.out.print("Please enter your number: ");
         int num = input.nextInt();
         boolean isPrime = isPrime(num);
